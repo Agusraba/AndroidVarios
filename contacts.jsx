c411fe3fd4e0b65d4082d70a036d6284b4f9bb98
Fausto Oliva <94145013+FaustoOliva@users.noreply.github.com>
2022-10-19 09:03:02 -0300

    Add files via upload

---
+++ contacts.jsx
@@ -0,0 +1,17 @@
+import * as Contacts from 'expo-contacts';
+
+useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(contact);
+        }
+      }
+    })();
+  }, []);
